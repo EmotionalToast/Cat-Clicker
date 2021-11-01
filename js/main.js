b5f5d6a3bf8e311025686d4d4e580303b60af024
EmotionalToast <kylezt3g@gmail.com>
2021-11-01 13:52:32 -0400

    Squashed commit of the following:
    commit b1656a16214a405e921e3c7f121f8b062c586b18
    Author: EmotionalToast <kylezt3g@gmail.com>
    Date:   Mon Nov 1 13:51:42 2021 -0400
    
        Update main.js
    
    commit 6c9b161bd06b8b2c2d397c36ae33d12c187fb669
    Author: EmotionalToast <kylezt3g@gmail.com>
    Date:   Mon Nov 1 13:47:54 2021 -0400
    
        Formulas

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
+//Set the buy spans
+let oneSpan = document.getElementById("oneSpan");
+let tenSpan = document.getElementById("tenSpan");
+let maxSpan = document.getElementById("maxSpan");
+
+//Set the initial buy span amount to one
+buyAmount('1');
+
+//Check if first time
+if(localStorage.getItem("cats") == null){
+    console.log("Created cats variable");
+
+    localStorage.setItem("cats", "0");
+    localStorage.setItem("clickMultipler", "1");
+
+    localStorage.setItem("mouses", "0");
+    localStorage.setItem("mouseCost", "10");
+    localStorage.setItem("mouseMultiplier", "1");
+
+    localStorage.setItem("yarn", "0");
+    localStorage.setItem("yarnCost", "500");
+    localStorage.setItem("yarnMultiplier", "1");
+}
+
+//Grab the items from storage
+let cats = parseFloat(localStorage.getItem("cats"));
+
+let mouses = parseFloat(localStorage.getItem("mouses"));
+let mouseCost = parseFloat(localStorage.getItem("mouseCost"));
+let mouseMultiplier = parseFloat(localStorage.getItem("mouseMultiplier"));
+
+let yarn = parseFloat(localStorage.getItem("yarn"));
+let yarnCost = parseFloat(localStorage.getItem("yarnCost"));
+let yarnMultiplier = parseFloat(localStorage.getItem("yarnMultiplier"));
+
+//Set functions
+setCats();
+setMouses();
+setYarn();
+
+
+
+//On load function
 window.onload = (event) => {
     if(localStorageTest() === true){
         console.log("LOCAL STORAGE IS ON!!!");
         document.getElementById("content").style = "display: flex;";
 
-        if(localStorage.getItem("cats") !== null){
-            gameLoop();
-        }
-        else{
-            console.log("Created cats variable");
-
-            localStorage.setItem("cats", "0");
-
-            localStorage.setItem("mouses", "0");
-            localStorage.setItem("mouseCost", "10");
-
-            localStorage.setItem("yarn", "0");
-            localStorage.setItem("yarnCost", "500"); 
-            
-            gameLoop();
-        }
-
-        cats = localStorage.getItem("cats");
-
-        mouses = localStorage.getItem("mouses");
-        mouseCost = localStorage.getItem("mouseCost");
-
-        yarn = localStorage.getItem("yarn");
-        yarnCost = localStorage.getItem("yarnCost");
-
-        setCats();
-        setMouses();
-        setYarn();
+        gameLoop();
 
         console.log("ALL LOADED!!!");
     }
@@ -41,19 +57,24 @@ window.onload = (event) => {
     }
 }
 
+
+
+
 //Game Functions
 function gameLoop(){
     window.setInterval(function(){
         if(localStorage.getItem("cats") !== null){
             console.log("GAME LOOP IS ON!!!");
-            getCats(mouses);
-            getCats(yarn * 2);
+
+            getCats(Math.floor((1 * mouses) * mouseMultiplier));
+            getCats(Math.floor((2 * yarn) * yarnMultiplier));
         }
-    }, 100);
+    }, 1000);
 }
 
+
 function getCats(newCats) {
-    cats = parseFloat(cats) + parseFloat(newCats);
+    cats = cats + newCats;
     setCats();
 }
 
@@ -62,24 +83,28 @@ function buyMouse(mouseBuyAmount){
         cats -= mouseCost;
         setCats();
 
-        mouses = parseFloat(mouses) + mouseBuyAmount;
-        mouseCost = Math.floor(mouseCost * 1.15);
+        mouses = mouses + mouseBuyAmount;
+        mouseCost = Math.floor(10 * Math.pow(1.15, mouses));
 
         setMouses();
     }
 }
+
 
 function buyYarn(yarnBuyAmount){
     if(cats >= yarnCost){
         cats -= yarnCost;
         setCats();
 
-        yarn = parseFloat(yarn) + yarnBuyAmount;
-        yarnCost = Math.floor(yarnCost * 1.5);
+        yarn = yarn + yarnBuyAmount;
+        yarnCost = Math.floor(500 * Math.pow(1.5, yarn));
 
         setYarn();
     }
 }
+
+
+
 
 //Set amounts for each item
 function setCats(){
@@ -102,6 +127,29 @@ function setYarn(){
     localStorage.setItem("yarn", yarn);
     localStorage.setItem("yarnCost", yarnCost);
 }
+
+
+function buyAmount(amountToBuy) {
+    if(amountToBuy == '1'){
+        oneSpan.style = "color: #9d1bd1";
+        tenSpan.style = "color: rgb(246, 254, 255)";
+        maxSpan.style = "color: rgb(246, 254, 255)";
+    }
+    else if(amountToBuy == '10'){
+        oneSpan.style = "color: rgb(246, 254, 255)";
+        tenSpan.style = "color: #9d1bd1";
+        maxSpan.style = "color: rgb(246, 254, 255)";
+    }
+    else if(amountToBuy == 'Max'){
+        oneSpan.style = "color: rgb(246, 254, 255)";
+        tenSpan.style = "color: rgb(246, 254, 255)";
+        maxSpan.style = "color: #9d1bd1";
+    }
+}
+
+
+
+
 
 //Check if local storage is turned on
 function localStorageTest(){
